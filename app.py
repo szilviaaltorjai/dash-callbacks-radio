f245573407691d72a6635b4162868cbb57eb850d
szilviaaltorjai <szilviaaltorjai@gmail.com>
2019-09-23 18:45:14 -0400

    cat

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from dash.dependencies import Input, Output, State
 
 myheading1='How to use callbacks'
 tabtitle = 'xkcd'
-list_of_options=['box plot', 'correlation', 'git commit', 'scatterplot']
-list_of_images=['outlier.png', 'correlation.png', 'gitcommit.jpg', 'scatterplot.png', 'good_code.png']
+list_of_options=['box plot', 'correlation', 'git commit', 'scatterplot', 'cat']
+list_of_images=['cat.png', 'outlier.png', 'correlation.png', 'gitcommit.jpg', 'scatterplot.png', 'good_code.png']
 sourceurl = 'https://xkcd.com/'
-githublink = 'https://github.com/austinlasseter/dash-callbacks-radio'
+githublink = 'https://github.com/szilviaaltorjai/dash-callbacks-radio'
 
 
 ########## Set up the chart
@@ -33,8 +33,9 @@ app.layout = html.Div(children=[
                 {'label':list_of_options[1], 'value':list_of_images[1]},
                 {'label':list_of_options[2], 'value':list_of_images[2]},
                 {'label':list_of_options[3], 'value':list_of_images[3]},
+                {'label':list_of_options[4], 'value':list_of_images[4]},
                 ],
-        value=list_of_images[4],
+        value=list_of_images[5],
         ),
     html.Div(id='your_output_here', children=''),
     html.Br(),
